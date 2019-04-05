c38fe49760b5d3cabb987dd66213ea4f1ffd1198
Dariell Vasquez <dariellv@yahoo.com>
2019-04-05 16:37:41 -0400

    plot MSE history using node-remote-plot

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 const LinearRegression = require('./linear-regression');
+const plot = require('node-remote-plot');
 
 let {features, labels, testFeatures, testLabels} = loadCSV('./cars.csv', {
     shuffle: true,
@@ -19,4 +20,9 @@ regression.train();
 
 // r^2 or Coefficient of Determination
 const r2 = regression.test(testFeatures, testLabels);
-console.log('R2 is : ', r2)
+
+plot({
+    x: regression.mseHistory,
+    xLabel: 'Iteration #',
+    yLabel: 'Mean Squared Error'
+})
