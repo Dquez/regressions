afd2eb3878fa1f2c24f96c0585d8f909c6fffa44
Dariell Vasquez <dariellv@yahoo.com>
2019-04-09 20:41:03 -0400

    train and recordMSE refactor to use crossEntropy formula

--- logistic-regression/logistic-regression.js
+++ logistic-regression/logistic-regression.js
@@ -5,9 +5,10 @@ class LogisticRegression {
     constructor(features, labels, options){
         this.features = this.processFeatures(features);
         this.labels = tf.tensor(labels);
-        this.options = Object.assign({learningRate: .1, iterations: 1000}, options)
+        this.options = Object.assign({learningRate: .1, iterations: 1000, decisionBoundary: 0.5}, options)
         this.weights = tf.zeros([this.features.shape[1],1]);
-        this.mseHistory = [];
+        // crossEntropy history
+        this.costHistory = [];
 
     }
     train() {
@@ -20,7 +21,7 @@ class LogisticRegression {
                 const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1])
                 this.gradientDescent(featureSlice, labelSlice);
             }
-            this.recordMSE();
+            this.recordCost();
             this.updateLearningRate();
         }
     }
@@ -35,7 +36,7 @@ class LogisticRegression {
         this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
     }
     test(testFeatures, testLabels) {
-        const predictions = this.predict(testFeatures).round();
+        const predictions = this.predict(testFeatures);
         testLabels = tf.tensor(testLabels);
 
         const incorrect = predictions.sub(testLabels).abs().sum().get();
@@ -53,7 +54,11 @@ class LogisticRegression {
         return features;
     }
     predict(observations) {
-        return this.processFeatures(observations).matMul(this.weights).sigmoid();
+        return this.processFeatures(observations)
+                .matMul(this.weights)
+                .sigmoid()
+                .greater(this.options.decisionBoundary)
+                .cast('float32');
     }
     standardize(features) {
         const { mean, variance } = tf.moments(features, 0);
@@ -62,23 +67,31 @@ class LogisticRegression {
 
         return features.sub(mean).div(variance.pow(0.5));
     }
-    recordMSE(){
-        const mse = this.features
-            .matMul(this.weights)
-            .sub(this.labels)
-            .pow(2)
-            .sum()
-            .div(this.features.shape[0])
-            .get();
-        
-        this.mseHistory.push(mse);
+    recordCost(){
+        const guesses = this.features.matMul(this.weights).sigmoid();
+        const termOne = this.labels.transpose().matMul(guesses.log());
+        const termTwo = this.labels
+            .mul(-1)
+            .add(1)
+            .transpose()
+            .matMul(
+                guesses
+                    .mul(-1)
+                    .add(1)
+                    .log()
+            );
+        const cost = termOne.add(termTwo)
+                .div(this.features.shape[0])
+                .mul(-1)
+                .get(0,0);
+        this.costHistory.unshift(cost);
     }
     updateLearningRate() {
-        const mseLength = this.mseHistory.length;
-        if(mseLength < 2) return;
+        const costLength = this.costHistory.length;
+        if(costLength < 2) return;
         
-        const lastValue = this.mseHistory[mseLength - 1];
-        const secondToLast = this.mseHistory[mseLength - 2];
+        const lastValue = this.costHistory[costLength - 1];
+        const secondToLast = this.costHistory[costLength - 2];
         if(lastValue > secondToLast){
             this.options.learningRate /= 2;
         } else {
