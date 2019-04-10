1ba8742f48c7cb75d6d8cede91039c8a5384a2ee
Dariell Vasquez <dariellv@yahoo.com>
2019-04-09 20:17:16 -0400

    test method checks percentage correct

--- logistic-regression/logistic-regression.js
+++ logistic-regression/logistic-regression.js
@@ -35,19 +35,12 @@ class LogisticRegression {
         this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
     }
     test(testFeatures, testLabels) {
-        testFeatures = this.processFeatures(testFeatures);
+        const predictions = this.predict(testFeatures).round();
         testLabels = tf.tensor(testLabels);
-        const predictions = testFeatures.matMul(this.weights);
-        
-        const res = testLabels.sub(predictions)
-            .pow(2)
-            .sum()
-            .get()
-        const tot = testLabels.sub(testLabels.mean())
-            .pow(2)
-            .sum()
-            .get()
-        return 1 - res / tot
+
+        const incorrect = predictions.sub(testLabels).abs().sum().get();
+        let numOfPredictions =  predictions.shape[0]
+        return (numOfPredictions - incorrect) / numOfPredictions;
     }
     processFeatures(features){
         features = tf.tensor(features);
