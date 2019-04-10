ef881fc80310d609455838bd3ecb7d8dc92a8bf0
Dariell Vasquez <dariellv@yahoo.com>
2019-04-10 16:53:26 -0400

    label encoding, mpg broken into low, medium and high efficiency

---
+++ multinominal-logistic-regression.1/index.js
@@ -0,0 +1,37 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const _ = require('lodash');
+const loadCSV = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
+const plot = require('node-remote-plot');
+
+let {features, labels, testFeatures, testLabels} = loadCSV('../data/cars.csv', {
+    shuffle: true,
+    splitTest: 50,
+    dataColumns: ['horsepower', 'displacement', 'weight'],
+    labelColumns: ['mpg'],
+    converters: {
+        mpg: value => {
+            const mpg = parseFloat(value);
+            // The mpg continuous values are broken into three groups, low medium and high fuel efficiency, marked by an array
+            return  mpg < 15 ? [1,0,0] :
+                    mpg < 30 ? [0,1,0] :
+                    [0,0,1]
+        }
+    }
+})
+
+const regression = new LogisticRegression(features, _.flatMap(labels), {
+    learningRate: 0.5,
+    iterations: 10,
+    batchSize: 10
+})
+
+regression.train();
+
+console.log(regression.test(testFeatures, testLabels));
+
+plot({
+    x: regression.costHistory.reverse(),
+    xLabel: '# of iterations'
+})
