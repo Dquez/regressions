f43f8a314b9a4f5e9224d26a7e6ff909cabcf478
Dariell Vasquez <dariellv@yahoo.com>
2019-04-19 10:48:52 -0400

    image classification memory usage refactor

--- image-classification-regression/index.js
+++ image-classification-regression/index.js
@@ -4,19 +4,24 @@ const _ = require('lodash');
 const LogisticRegression = require('./logistic-regression');
 const mnist = require('mnist-data');
 
-// grab the first ten numbers from training data
-const mnistData = mnist.training(0,5000);
 
-// map over numbers to create 1 condensed array for each number with corresponding grayscale values.
-const features = mnistData.images.values.map(image => _.flatMap(image));
-// encode labels to have a 1 for the corresponding label
-const encodedLabels = mnistData.labels.values.map(label=> {
-    const row = new Array(10).fill(0);
-    row[label] = 1;
-    return row;
-})
+function loadData(){
+    // grab the first ten numbers from training data
+    const mnistData = mnist.training(0,60000);
 
-const regression = new LogisticRegression(features, encodedLabels, {
+    // map over numbers to create 1 condensed array for each number with corresponding grayscale values.
+    const features = mnistData.images.values.map(image => _.flatMap(image));
+    // encode labels to have a 1 for the corresponding label
+    const encodedLabels = mnistData.labels.values.map(label=> {
+        const row = new Array(10).fill(0);
+        row[label] = 1;
+        return row;
+    })
+    return {features, labels: encodedLabels};
+}
+
+const {features, labels} = loadData();
+const regression = new LogisticRegression(features, labels, {
     learningRate: 1,
     iterations: 20,
     batchSize: 100
@@ -24,7 +29,7 @@ const regression = new LogisticRegression(features, encodedLabels, {
 
 regression.train();
 
-const testMnistData = mnist.testing(0,100);
+const testMnistData = mnist.testing(0,1000);
 const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
 const testEncodedLabels = testMnistData.labels.values.map(label=> {
     const row = new Array(10).fill(0);
