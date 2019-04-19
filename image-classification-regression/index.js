950b035a5146c20c4036a78ad6e6f37a4745e492
Dariell Vasquez <dariellv@yahoo.com>
2019-04-18 23:44:09 -0400

    use flatmap to condense grayscale values and encode label values

--- image-classification-regression/index.js
+++ image-classification-regression/index.js
@@ -5,3 +5,17 @@ const loadCSV = require('../load-csv');
 const LogisticRegression = require('./logistic-regression');
 const plot = require('node-remote-plot');
 const mnist = require('mnist-data');
+
+// grab the first ten numbers from training data
+const mnistData = mnist.training(0,10);
+
+// map over numbers to create 1 condensed array for each number with corresponding grayscale values.
+const features = mnistData.images.values.map(image => _.flatMap(image));
+// encode labels to have a 1 for the corresponding label
+const encodedLabels = mnistData.labels.values.map(label=> {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+})
+
+console.log(encodedLabels);
