d8540278db96826fc15ecf9fdf3ee31d02be7ee3
Dariell Vasquez <dariellv@yahoo.com>
2019-04-18 23:52:26 -0400

    add test set to test the accuracy of predictions

--- image-classification-regression/index.js
+++ image-classification-regression/index.js
@@ -1,13 +1,11 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const _ = require('lodash');
-const loadCSV = require('../load-csv');
 const LogisticRegression = require('./logistic-regression');
-const plot = require('node-remote-plot');
 const mnist = require('mnist-data');
 
 // grab the first ten numbers from training data
-const mnistData = mnist.training(0,10);
+const mnistData = mnist.training(0,1000);
 
 // map over numbers to create 1 condensed array for each number with corresponding grayscale values.
 const features = mnistData.images.values.map(image => _.flatMap(image));
@@ -18,4 +16,21 @@ const encodedLabels = mnistData.labels.values.map(label=> {
     return row;
 })
 
-console.log(encodedLabels);
+const regression = new LogisticRegression(features, encodedLabels, {
+    learningRate: 1,
+    iterations: 5,
+    batchSize: 100
+})
+
+regression.train();
+
+const testMnistData = mnist.testing(0,100);
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label=> {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+})
+
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('Accuracy is ', accuracy);
