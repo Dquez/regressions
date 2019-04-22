85f0aa9d34373413d5de061424733e948e7aa7ab
Dariell Vasquez <dariellv@yahoo.com>
2019-04-21 23:18:55 -0400

    map over each element in non header rows and convert to float

--- loadCSV/load-csv-dummy.js
+++ loadCSV/load-csv-dummy.js
@@ -1,7 +1,21 @@
 const fs = require('fs');
+const _ = require('lodash');
 function loadCSV(filename, options){
-    const data = fs.readFileSync(filename, {encoding: 'utf-8'})
-    console.log(data.split('\n').map(row => row.split(',')))
-   
-}
+    let data = fs.readFileSync(filename, {encoding: 'utf-8'})
+    // split on each new line of the csv and make an array of arrays for each element
+    data = data.split('\n').map(row => row.split(','))
+    // drop trailing empty values in each row
+    data.map(row => _.dropRightWhile(row, val => val === ''))
+    const headers = _.first(data);
+
+    data = data.map((row,index)=> {
+        // row is a header row
+        if(index === 0) return row;
+        return row.map((element, index) => {
+            const result = parseFloat(element);
+            return _.isNaN(result) ? element : result;
+        })
+    })
+    console.log(data);
+}   
 loadCSV('data.csv');
