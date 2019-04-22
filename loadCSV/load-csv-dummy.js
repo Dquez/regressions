ced38b5962c18dafbc1b1830f8de7dd13e219603
Dariell Vasquez <dariellv@yahoo.com>
2019-04-21 23:10:10 -0400

    create fake csv loader to clarify load-csv file

---
+++ loadCSV/load-csv-dummy.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+function loadCSV(filename, options){
+    const data = fs.readFileSync(filename, {encoding: 'utf-8'})
+    console.log(data.split('\n').map(row => row.split(',')))
+   
+}
+loadCSV('data.csv');
